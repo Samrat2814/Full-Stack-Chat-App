25dffb4b27891cea7bf64cb6e0db2d71cd7f5d4f
Samrat2814 <aslamhossainsamrat@gmail.com>
2025-05-03 08:52:50 +0600

    bug fixed now

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import { Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./Pages/HomePage"
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./Pages/SettingsPage";
